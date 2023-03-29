92d39ba1fe5ac46490feab48817ff446a890a922
Lukas Volk <lukas.volk2014@gmail.com>
2023-03-29 13:29:57 +0200

    added

--- src/agt/personal_assistant.asl
+++ src/agt/personal_assistant.asl
@@ -13,7 +13,15 @@
 */
 @start_plan
 +!start : true <-
-    .print("Hello world").
+    .print("Hello world");
+    !setupDweet;
+    .wait(2000);
+    publish("Hello world!").
+
+@setup_dweet_artifact_plan
++!setupDweet : true <- 
+    makeArtifact("dweet","room.DweetArtifact",[], DweetId);
+    .print("Made artifact").
 
 /* Import behavior of agents that work in CArtAgO environments */
 { include("$jacamoJar/templates/common-cartago.asl") }
--- src/env/room/DweetArtifact.java
+++ src/env/room/DweetArtifact.java
@@ -1,11 +1,47 @@
 package room;
 
 import cartago.Artifact;
+import cartago.OPERATION;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 /**
  * A CArtAgO artifact that provides an operation for sending messages to agents 
  * with KQML performatives using the dweet.io API
  */
 public class DweetArtifact extends Artifact {
-    
+
+    private static final String DWEET_URI = "https://dweet.io/dweet/for/lukas-was";
+
+    void init() {
+
+    }
+
+    @OPERATION
+    void publish(String message) {
+
+        final var dweet = """
+                {"message": "%s"}
+                """.formatted(message);
+
+        System.out.println(dweet);
+        final var client = HttpClient.newHttpClient();
+        final var request = HttpRequest.newBuilder(URI.create(DWEET_URI))
+                .POST(HttpRequest.BodyPublishers.ofString(dweet))
+                .header("Content-Type", "application/json")
+                .build();
+        try {
+            final var send = client.send(request, HttpResponse.BodyHandlers.discarding());
+            if(send.statusCode() > 200) {
+                System.out.println(send.statusCode());
+                System.out.println("Something went wrong");
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
